eb9801f8a899d5408a79a43513f02ae77b53dd6d
Michael Vanin <67514946+devmichaelvanin@users.noreply.github.com>
2020-11-13 10:12:47 -0330

    Add files via upload

---
+++ javascript-clock/app.js
@@ -0,0 +1,20 @@
+const secondHand = document.querySelector('.seconds-hand');
+const minutesHand = document.querySelector('.minutes-hand');
+const hoursHand = document.querySelector('.hours-hand');
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = (minutes / 60) * 360;
+  minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegrees = (hours / 12) * 360;
+  hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
